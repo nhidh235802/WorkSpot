f14a47cfa171b932339434e0e444f07b9c5c0ef0
nhidh235802 <duongnhi2932@gmail.com>
2026-05-06 00:12:15 +0700

    Cài đặt Migration

---
+++ backend/src/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'admin',
+  database: process.env.DB_DATABASE || 'workspot_db',
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
+  synchronize: false,
+});
